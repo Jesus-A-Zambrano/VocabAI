47be1963cdf6d4b580c27de8f08e04ce18e3f249
Se-Basstian <p.sebastian.23.11.99@gmail.com>
2025-05-21 13:41:15 -0500

    feat(layout): integrar HeaderLayout y FooterLayout en DefaultLayout

--- apps/frontend/src/components/layout/DefaultLayout.tsx
+++ apps/frontend/src/components/layout/DefaultLayout.tsx
@@ -1,9 +1,13 @@
 import { Outlet } from "react-router";
+import HeaderLayout from "./Home/HeaderLayout";
+import FooterLayout from "./Home/FooterLayout";
 
 const DefaultLayout = () => {
   return (
     <>
+      <HeaderLayout />
       <Outlet />
+      <FooterLayout />
     </>
   );
 };
