6194711760b5e9b5a37e0f095750974a1e6698b3
Se-Basstian <p.sebastian.23.11.99@gmail.com>
2025-05-21 22:44:34 -0500

    feat(layout): agregar navegación al login desde el botón de BannerLayout

--- apps/frontend/src/components/layout/Home/BannerLayout.tsx
+++ apps/frontend/src/components/layout/Home/BannerLayout.tsx
@@ -1,30 +1,39 @@
 import ButtonHome from "../../ui/ButtonHome";
 import imagenCardBanner from "../../../assets/imagenes/Tarejeta de Banner.png";
 import { cn } from "clsx-for-tailwind";
+import { useNavigate } from "react-router";
 
 const BannerLayout = () => {
-	return (
-		<article className={cn("bg-orange-100 flex justify-center py-10")}>
-			<section className={cn("w-[1060px] h-full px-[15px]", "flex gap-5")}>
-				<div className={cn("flex flex-col justify-around items-center")}>
-					<div>
-						<h2 className={cn("text-5xl font-semibold")}>
-							Aprendiendo nuevas palabras con ayuda de
-							<span className={cn("text-orange-500")}> VocabIA</span>
-						</h2>
-						<p className={cn("text-2xl text-black/60 mt-2")}>
-							Tarjetas de memoria personalizadas según tu nivel y preferencias
-							para dominar cualquier idioma.
-						</p>
-					</div>
+  const navigate = useNavigate();
 
-					<ButtonHome secundario>Emprecemos a Aprender</ButtonHome>
-				</div>
+  const handleButtonIniciarSesion = () => {
+    navigate("/login");
+  };
 
-				<img src={imagenCardBanner} alt="Imagen de Card de la app" />
-			</section>
-		</article>
-	);
+  return (
+    <article className={cn("bg-orange-100 flex justify-center py-10")}>
+      <section className={cn("w-[1060px] h-full px-[15px]", "flex gap-5")}>
+        <div className={cn("flex flex-col justify-around items-center")}>
+          <div>
+            <h2 className={cn("text-5xl font-semibold")}>
+              Aprendiendo nuevas palabras con ayuda de
+              <span className={cn("text-orange-500")}> VocabIA</span>
+            </h2>
+            <p className={cn("text-2xl text-black/60 mt-2")}>
+              Tarjetas de memoria personalizadas según tu nivel y preferencias
+              para dominar cualquier idioma.
+            </p>
+          </div>
+
+          <ButtonHome secundario onClick={handleButtonIniciarSesion}>
+            Emprecemos a Aprender
+          </ButtonHome>
+        </div>
+
+        <img src={imagenCardBanner} alt="Imagen de Card de la app" />
+      </section>
+    </article>
+  );
 };
 
 export default BannerLayout;
