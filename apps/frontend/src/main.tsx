042ed99abcfd7af5c32ae782046a5aa417b64ad8
Se-Basstian <p.sebastian.23.11.99@gmail.com>
2025-05-21 22:46:16 -0500

    feat(auth): integrar ClerkProvider para manejar autenticación global

--- apps/frontend/src/main.tsx
+++ apps/frontend/src/main.tsx
@@ -3,13 +3,23 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import { BrowserRouter } from "react-router";
+import { ClerkProvider } from "@clerk/clerk-react";
 
 const root = document.getElementById("root") as HTMLElement;
 
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+// console.log(PUBLISHABLE_KEY);
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error("Missing Publishable key");
+}
+
 createRoot(root).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl={"/"}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ClerkProvider>
   </StrictMode>,
 );
