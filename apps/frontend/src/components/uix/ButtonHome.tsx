3cf007fc554d7b93ad1bcc4f0afe34471b216bab
Leodan Valda <lvalda.dev@gmail.com>
2025-05-23 17:33:15 -0400

    fix: intento 3 vercel

---
+++ apps/frontend/src/components/uix/ButtonHome.tsx
@@ -0,0 +1,35 @@
+import type { ReactNode } from "react";
+import { cn } from "clsx-for-tailwind";
+
+interface Props {
+  secundario?: boolean;
+  children: ReactNode;
+  type?: "button" | "submit" | "reset";
+  onClick?: () => void;
+}
+const ButtonHome = ({
+  secundario = false,
+  children,
+  type = "button",
+  onClick,
+}: Props) => {
+  return (
+    <button
+      type={type}
+      onClick={onClick}
+      className={cn(
+        "px-4 py-2 rounded font-bold",
+        "transition-all",
+        "hover:shadow-md hover:scale-105",
+        {
+          "bg-orange-200 text-orange-600": !secundario,
+          "bg-orange-500 text-white": secundario,
+        },
+      )}
+    >
+      {children}
+    </button>
+  );
+};
+
+export default ButtonHome;
---
+++ apps/frontend/src/components/uix/Card.tsx
@@ -0,0 +1,28 @@
+type CardColor = 'orange' | 'blue' | 'green' | 'purple' | 'red';
+
+const colorClasses: Record<CardColor, string> = {
+    orange: "hover:bg-orange-100 border-t-orange-600",
+    blue: "hover:bg-blue-100 border-t-blue-600",
+    green: "hover:bg-green-100 border-t-green-600",
+    purple: "hover:bg-purple-100 border-t-purple-600",
+    red: "hover:bg-red-100 border-t-red-600",
+};
+
+interface CardProps {
+    color: CardColor;
+    titulo: string;
+    mensaje: string;
+}
+
+const Card: React.FC<CardProps> = ({ color, titulo, mensaje }) => {
+    const selectedColorClasses = colorClasses[color];
+
+    return (
+        <div className={`max-w-90 border-t-8 border-gray-200 p-7 rounded-lg transition-colors bg-white w-full ${selectedColorClasses}`}>
+            <h3 className="font-bold text-gray-800 mb-2 text-xl">{titulo}</h3>
+            <p className="text-base text-gray-600 font-medium">{mensaje}</p>
+        </div>
+    );
+};
+
+export default Card;
